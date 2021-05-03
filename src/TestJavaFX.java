09fc670bf620fd4108571d2f7e5867ac5ce17a78
josephwang3 <josephxawang@gmail.com>
2021-05-03 10:57:01 -0500

    Submitted final project

--- src/TestJavaFX.java
+++ src/TestJavaFX.java
@@ -1,12 +1,29 @@
+import java.io.File;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class TestJavaFX extends Application {
+
+    private static final String MEDIA_URL = "http://liveexample.pearsoncmg.com/common/sample.mp4";
+
     // override start method in Application class
     @Override 
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
+
+        /*Media media = new Media(MEDIA_URL);
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();*/
+
+        java.net.URL resource = getClass().getResource("organfinale.mp3");
+        Media media = new Media(resource.toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
+
+        
         // create a scene and place a button in the scene
         Button btOK = new Button("OK");
         Scene scene = new Scene(btOK, 200, 250);
@@ -19,6 +36,11 @@ public class TestJavaFX extends Application {
 
         // display stage
         primaryStage.show();
+
+        /*String musicFileName = "organfinale.mp3";
+        Media media = new Media("file://C:/Users/Work/OneDrive - LeTourneau University/15 LETU/21S/COSC 2103 CS 2/Java/FinalProject/JavaFXTest/src/organfinale.mp3");
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();*/
     }
 
     public static void main(String[] args) {
--- src/TextEditor.java
+++ src/TextEditor.java
@@ -1,23 +1,31 @@
+import java.awt.Toolkit;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.PrintWriter;
+
+import java.util.Optional;
 import java.util.Scanner;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.FileChooser.ExtensionFilter;
 
 public class TextEditor extends Application {
+    int lineNumber = 1;
+    VBox lineNumbers = new VBox();
+
     /** method to create the BorderPane */
     public BorderPane getPane(Stage stage) {
         // create horizonal box for options with horizontal gap of 15 and padding of 5 all around
@@ -34,17 +42,14 @@ public class TextEditor extends Application {
         options.getChildren().addAll(btnNew, btnOpen, btnSave);
         
         // create vertical box for line numbers with vertical gap of 15
-        VBox lineNumbers = new VBox();
         lineNumbers.setPadding(new Insets(5, 5, 0, 5));
 
-        // create labels for line numbers 1 - 10
-        for (int i = 1; i < 10; i++) {
-            lineNumbers.getChildren().add(new Label(String.valueOf(i)));
-        }
+        // add line number for line 1
+        lineNumbers.getChildren().add(new Label(String.valueOf(lineNumber)));
+        lineNumber = lineNumber + 1;
 
         // create text area
         TextArea textArea = new TextArea();
-        //textArea.setPadding(new Insets(5, 5, 5, 5));
 
         // create overall BorderPane
         BorderPane pane = new BorderPane();
@@ -52,6 +57,14 @@ public class TextEditor extends Application {
         pane.setTop(options);
         pane.setLeft(lineNumbers);
         pane.setCenter(textArea);
+
+        // add line numbers
+        textArea.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.ENTER) {
+                lineNumbers.getChildren().add(new Label(String.valueOf(lineNumber)));
+                lineNumber = lineNumber + 1;        
+            }
+        });
 
         // when new button pressed, open new window
         btnNew.setOnAction(e -> {
@@ -65,15 +78,14 @@ public class TextEditor extends Application {
 
         // create a FileChooser
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setInitialDirectory(new File("C:\\Users\\Work\\OneDrive - LeTourneau University\\15 LETU\\21S\\COSC 2103 CS 2\\Java\\FinalProject\\JavaFXTest\\src"));
-        //fileChooser.setSelectedExtensionFilter(new ExtensionFilter("Text Files", "*.txt"));
+        //fileChooser.setInitialDirectory(new File("C:\\Users\\Work\\OneDrive - LeTourneau University\\15 LETU\\21S\\COSC 2103 CS 2\\Java\\FinalProject\\JavaFXTest\\src"));
 
         // set open button to open windows explorer to open a file
         btnOpen.setOnAction(e -> {
             try {
                 // get file from FileChooser
                 File openFile = fileChooser.showOpenDialog(stage);
-
+                
                 // create Scanner to read file
                 Scanner input = new Scanner(openFile);
 
@@ -129,6 +141,31 @@ public class TextEditor extends Application {
 
         // display stage
         primaryStage.show();
+
+        // show close confirmation window
+        primaryStage.setOnCloseRequest(e -> {
+            // play default windows sound using AWT
+            Toolkit.getDefaultToolkit().beep();
+
+            // create confirmation alert
+            Alert closeConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
+            closeConfirmation.setTitle("Confirm Close");
+            closeConfirmation.setHeaderText(null);
+            closeConfirmation.setContentText("Are you sure you want to close this window?");
+
+            // OK and Cancel buttons for confirmation window
+            ButtonType btnTypeOK = new ButtonType("OK");
+            ButtonType btnTypeCancel = new ButtonType("Cancel");
+            closeConfirmation.getButtonTypes().setAll(btnTypeOK, btnTypeCancel);
+
+            // track results of close confirmation box
+            Optional<ButtonType> result = closeConfirmation.showAndWait();
+
+            // for cancel button, don't close window
+            if (result.isPresent() && result.get() == btnTypeCancel) {
+                e.consume();
+            }
+        });
     }
 
     public static void main(String[] args) {
